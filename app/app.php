c5c78ad9321762c264b2006fe32c10c61d36d8bf
Ike Mattice <gogreen37@me.com>
2015-08-28 12:04:41 -0700

    add add_store route to all_stores page.

--- app/app.php
+++ app/app.php
@@ -19,17 +19,29 @@
     Request::enableHttpMethodParameterOverride();
 
 //landing page
+    //renders homepage
     $app->get("/", function() use ($app)
     {
         return $app['twig']->render('home.html.twig');
     });
 
-    //directs to a list of all stores
+//all stores page
+    //directs to all_stores page
     $app->get('all_stores', function() use ($app)
     {
         return $app['twig']->render('all_stores.html.twig', array('stores' => Store::getAll()));
     });
 
+    //allows user to add stores to page
+    $app->post('/add_stores', function() use ($app)
+    {
+        $store_name = $_POST['store_name'];
+        $store = new Store($store_name);
+        $store->save();
+        return $app['twig']->render('all_stores.html.twig', array('stores' => Store::getAll()));
+    });
+
+//all brands page
     //directs to a list of all brands
     $app->get('all_brands', function() use ($app)
     {
