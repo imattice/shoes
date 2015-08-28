b9c663d7dd7b28318b4fabf5e4c3144533da2979
Ike Mattice <gogreen37@me.com>
2015-08-28 10:13:09 -0700

    add test_save(), test_getAll(), and test_deleteAll() to BrandTest.php.

---
+++ src/Brand.php
@@ -0,0 +1,31 @@
+<?php
+    class Brand
+    {
+        private $id;
+        private $brand_name;
+
+//Brand object constructor
+        function __construct($brand_name, $id = null)
+        {
+            $this->brand_name = $brand_name;
+            $this->id = $id;
+        }
+
+//setters
+        function setBrandName($new_brand_name)
+        {
+            $this->brand_name = (string) $new_brand_name;
+        }
+
+//getters
+        function getBrandName()
+        {
+            return $this->brand_name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+    }
+?>
--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -1,1 +1,70 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
 
+    require_once "src/Store.php";
+    require_once "src/Brand.php";
+
+// server signin
+    $server = 'mysql:host=localhost;dbname=shoes_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class BrandTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+            Store::deleteAll();
+            Brand::deleteAll();
+        }
+
+        //test for save()
+                function test_save()
+                {
+                    $brand_name = 'Nike';
+                    $test_brand = new Brand($brand_name);
+
+                    $test_brand->save();
+                    $result = Brand::getAll();
+
+                    $this->assertEquals($test_brand, $result[0]);
+                }
+
+        //test for getAll()
+                function test_getAll()
+                {
+                    $brand_name = 'Nike';
+                    $test_brand = new Brand($brand_name);
+                    $test_brand->save();
+
+                    $brand_name2 = 'Adidas';
+                    $test_brand2 = new Brand($brand_name2);
+                    $test_brand2->save();
+
+                    $result = Brand::getAll();
+
+                    $this->assertEquals([$test_brand, $test_brand2], $result);
+                }
+
+        //test for deleteAll()
+                function test_deleteAll()
+                {
+                    $brand_name = 'Nike';
+                    $test_brand = new Brand($brand_name);
+                    $test_brand->save();
+
+                    $brand_name2 = 'Adidas';
+                    $test_brand2 = new Brand($brand_name2);
+                    $test_brand2->save();
+
+                    Brand::deleteAll();
+                    $result = Brand::getAll();
+
+                    $this->assertEquals([], $result);
+                }
+    }
+?>
